603fe3ccd6bb35ee6fd9b83386511653883094f2
sedooj <sergo54ru@gmail.com>
2024-03-18 09:15:28 +0700

    Initial commit

---
+++ app/src/main/java/com/sedooj/resumen/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.sedooj.resumen
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sedooj.resumen.ui.theme.ResumenTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ResumenTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+
+                }
+            }
+        }
+    }
+}
