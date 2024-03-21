b9e753f8508f9a2afac1dc1663122f8f33c44bb9
sedooj <sergo54ru@gmail.com>
2024-03-21 13:46:30 +0700

    Updated AGP version to 8.3.1 Updated kotlinCompiler version to 1.5.1

--- app/src/main/java/com/sedooj/resumen/navigation/graph.kt
+++ app/src/main/java/com/sedooj/resumen/navigation/graph.kt
@@ -1,5 +1,8 @@
 package com.sedooj.resumen.navigation
 
+import com.ramcosta.composedestinations.annotation.NavGraph
+import com.ramcosta.composedestinations.annotation.RootGraph
+
 @NavGraph<RootGraph>(
     route = "preferred_route",
     start = true,
