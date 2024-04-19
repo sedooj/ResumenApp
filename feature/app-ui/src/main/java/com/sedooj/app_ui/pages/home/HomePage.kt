f0283ecabea84a42ce6e112cf71313fcd89572b0
sedooj <sergo54ru@gmail.com>
2024-04-19 18:40:26 +0700

    Created CheckButton ui component
    Signed-off-by: sedooj <sergo54ru@gmail.com>

--- feature/app-ui/src/main/java/com/sedooj/app_ui/pages/home/HomePage.kt
+++ feature/app-ui/src/main/java/com/sedooj/app_ui/pages/home/HomePage.kt
@@ -8,13 +8,11 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.sedooj.app_ui.navigation.config.ScreensTransitions
 import com.sedooj.app_ui.pages.Routes
-import com.sedooj.arch.viewmodel.auth.HomeViewModel
 import com.sedooj.ui_kit.FilledButton
 import com.sedooj.ui_kit.R.string
 import com.sedooj.ui_kit.Screen
@@ -24,7 +22,6 @@ import kotlinx.coroutines.launch
 @Composable
 fun HomeScreen(
     destinationsNavigator: DestinationsNavigator,
-    homeViewModel: HomeViewModel = hiltViewModel(),
 ) {
     val scope = rememberCoroutineScope()
     Screen(
@@ -38,7 +35,7 @@ fun HomeScreen(
             label = stringResource(id = string.create_resume),
             onClick = {
                 scope.launch {
-
+                    destinationsNavigator.navigate(Routes.CREATE_RESUME)
                 }
             })
     }
---
+++ feature/app-ui/src/main/java/com/sedooj/app_ui/pages/resume/create/CreateResumePage.kt
@@ -0,0 +1,80 @@
+package com.sedooj.app_ui.pages.resume.create
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.annotation.RootGraph
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.sedooj.app_ui.navigation.config.ScreensTransitions
+import com.sedooj.app_ui.pages.Routes
+import com.sedooj.arch.viewmodel.auth.resume.CreateResumeViewModel
+import com.sedooj.ui_kit.R
+import com.sedooj.ui_kit.Screen
+
+@Destination<RootGraph>(
+    start = false,
+    route = Routes.CREATE_RESUME,
+    style = ScreensTransitions::class
+)
+@Composable
+fun CreateResumePage(
+    destinationsNavigator: DestinationsNavigator,
+    createResumeViewModel: CreateResumeViewModel = hiltViewModel(),
+) {
+    Screen(
+        title = stringResource(id = R.string.app_name),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        alignment = Alignment.Top
+    ) {
+        ResumeOptionsComponent(createResumeViewModel = createResumeViewModel)
+    }
+}
+
+@Composable
+fun ResumeOptionsComponent(
+    createResumeViewModel: CreateResumeViewModel,
+) {
+    val uiState = createResumeViewModel.uiState.collectAsState().value
+    TitleTextField(onValueChange = {
+        createResumeViewModel.updateTitle(input = it)
+    }, value = uiState.title ?: "")
+}
+
+@Composable
+private fun TitleTextField(
+    onValueChange: (String) -> Unit,
+    value: String,
+) {
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = value,
+        onValueChange = {
+            onValueChange(it)
+        },
+        label = {
+            Text(text = stringResource(id = R.string.resume_name))
+        },
+        singleLine = true,
+        maxLines = 1,
+        shape = RoundedCornerShape(10.dp),
+        isError = false,
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Next
+        )
+    )
+}
