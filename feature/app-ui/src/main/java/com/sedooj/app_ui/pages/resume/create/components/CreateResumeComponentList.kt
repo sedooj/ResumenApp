9775fea8031122206a1e130bb68cf0958ed2cb7b
sedooj <sergo54ru@gmail.com>
2024-04-24 11:40:14 +0700

    Code refactor

--- feature/app-ui/src/main/java/com/sedooj/app_ui/pages/resume/create/components/CreateResumeComponentList.kt
+++ feature/app-ui/src/main/java/com/sedooj/app_ui/pages/resume/create/components/CreateResumeComponentList.kt
@@ -1,21 +1,11 @@
 package com.sedooj.app_ui.pages.resume.create.components
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.ramcosta.composedestinations.annotation.Destination
-import com.ramcosta.composedestinations.annotation.RootGraph
-import com.ramcosta.composedestinations.navigation.DestinationsNavigator
-import com.sedooj.app_ui.navigation.config.ScreensTransitions
-import com.sedooj.app_ui.pages.Routes
 import com.sedooj.arch.viewmodel.auth.model.TabsModel
 import com.sedooj.ui_kit.FilledButton
-import com.sedooj.ui_kit.Screen
 
 @Composable
 fun CreateResumeComponentsPage(
