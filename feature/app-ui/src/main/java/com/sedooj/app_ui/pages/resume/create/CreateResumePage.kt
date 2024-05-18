fafbf57bfaa15b0e661039f89db5866f8374d3b7
sedooj <sergo54ru@gmail.com>
2024-05-18 18:36:52 +0700

    Created DoneBox for CreateResumePage
    Signed-off-by: sedooj <sergo54ru@gmail.com>

--- feature/app-ui/src/main/java/com/sedooj/app_ui/pages/resume/create/CreateResumePage.kt
+++ feature/app-ui/src/main/java/com/sedooj/app_ui/pages/resume/create/CreateResumePage.kt
@@ -1,11 +1,15 @@
 package com.sedooj.app_ui.pages.resume.create
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -15,6 +19,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
@@ -28,12 +33,12 @@ import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.ramcosta.composedestinations.spec.Direction
 import com.sedooj.app_ui.navigation.config.SlideScreenTransitions
 import com.sedooj.app_ui.pages.resume.create.components.CreateResumeComponentsPage
+import com.sedooj.app_ui.pages.resume.create.data.DoneBox
 import com.sedooj.arch.Routes
 import com.sedooj.arch.viewmodel.auth.resume.CreateResumeViewModel
 import com.sedooj.ui_kit.R.drawable
 import com.sedooj.ui_kit.R.string
 import com.sedooj.ui_kit.components.LostDataAlert
-import com.sedooj.ui_kit.fields.FilledButton
 import com.sedooj.ui_kit.screens.EditableTitleScreen
 import kotlinx.coroutines.launch
 
@@ -48,8 +53,6 @@ fun CreateResumePage(
     createResumeViewModel: CreateResumeViewModel,
 ) {
     var isAlertDialogVisible by remember { mutableStateOf(false) }
-    var isDataSaved by remember { mutableStateOf(false) }
-    var isDataEdited by remember { mutableStateOf(false) }
     val viewModel = createResumeViewModel.uiState.collectAsState().value
     val isResumeFilled by remember {
         mutableStateOf(
@@ -103,14 +106,6 @@ fun CreateResumePage(
         },
         showAlert = isAlertDialogVisible,
         actionButton = {
-            if (isResumeFilled)
-                FilledButton(
-                    label = stringResource(id = string.done),
-                    onClick = {
-                        scope.launch {
-                            createResumeViewModel.push()
-                        }
-                    })
             IconButton(onClick = { focusManager.moveFocus(FocusDirection.Left) }) {
                 Icon(
                     painter = painterResource(id = drawable.edit_pen),
@@ -127,6 +122,24 @@ fun CreateResumePage(
                 destinationsNavigator.navigate(it)
             }
         )
+        if (isResumeFilled) {
+            DoneBox(
+                onClick = {
+                    scope.launch {
+                        createResumeViewModel.push()
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(15.dp)
+                    .border(
+                        1.dp,
+                        color = MaterialTheme.colorScheme.primary,
+                        shape = RoundedCornerShape(10.dp)
+                    )
+                    .clip(shape = RoundedCornerShape(10.dp))
+            )
+        }
     }
 
 }
--- feature/app-ui/src/main/java/com/sedooj/app_ui/pages/resume/create/data/CreateResumeComponent.kt
+++ feature/app-ui/src/main/java/com/sedooj/app_ui/pages/resume/create/data/CreateResumeComponent.kt
@@ -1,12 +1,57 @@
 package com.sedooj.app_ui.pages.resume.create.data
 
-class CreateResumeComponent {
-}
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.sedooj.ui_kit.R.string
+import com.sedooj.ui_kit.fields.FilledButton
 
-class CreateResumeComponentData {
-
-}
-
-private class CreateResumeComponentContent {
-
+@Composable
+fun DoneBox(onClick: () -> Unit, modifier: Modifier = Modifier) {
+    Box(modifier = modifier, contentAlignment = Alignment.Center) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp),
+            verticalArrangement = Arrangement.spacedBy(
+                10.dp,
+                alignment = Alignment.CenterVertically
+            ),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Done, contentDescription = stringResource(
+                    id = string.resume_creation_done
+                ),
+                modifier = Modifier
+                    .size(40.dp)
+                    .background(color = Color(26, 170, 15, 255), shape = CircleShape).padding(5.dp),
+                tint = Color.White
+            )
+            Text(
+                text = stringResource(id = string.resume_creation_done),
+                textAlign = TextAlign.Center
+            )
+            FilledButton(
+                label = stringResource(id = string.next),
+                onClick = onClick
+            )
+        }
+    }
 }
