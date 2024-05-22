b67a56253528eb7fe58eb8b2765ac8e58abddd31
sedooj <sergo54ru@gmail.com>
2024-05-22 18:07:05 +0700

    Changed app version
    Signed-off-by: sedooj <sergo54ru@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.sedooj.resumen"
         minSdk = 29
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "0.0.1-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
